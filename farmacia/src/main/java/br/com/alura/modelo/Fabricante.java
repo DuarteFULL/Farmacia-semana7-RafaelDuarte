f199940d9f0fbf4c3cf37b7ec6ecdb3237910e69
Rafael Duarte <rafael@prismarede.com.br>
2024-02-27 01:07:04 -0300

    Class Produto and Fabricante

---
+++ farmacia/src/main/java/br/com/alura/modelo/Fabricante.java
@@ -0,0 +1,33 @@
+package br.com.alura.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "fabricantes")
+public class Fabricante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	
+	public Fabricante() {
+	}
+	
+	public Fabricante(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
+
